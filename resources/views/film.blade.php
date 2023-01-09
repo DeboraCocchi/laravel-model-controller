83c416a9d317573bf35c55901222f0db3c6bc4d6
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-09 17:56:39 +0100

    [new-commit]with-images

--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -9,8 +9,9 @@
                 <div class="col-2 p-2">
                     <a href="{{ route('movie-details', $movie->id)}}">
                         <div class="card">
-                            <div class="card-body">
-                              <h5 class="card-title">{{$movie->title}}</h5>
+                            <img src="{{$movie->img}}" class="card-img-top" alt="{{$movie->title}}">
+                            <div class="card-body text-center d-flex flex-column justify-content-center">
+                              <h4 class="card-title">{{$movie->title}}</h4>
                               <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
                               <p class="card-text">{{$movie->nationality}} || {{$movie->vote}}</p>
                             </div>
--- resources/views/movie-details.blade.php
+++ resources/views/movie-details.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="container-details">
     <div class="card">
+        <img src="{{$movie->img}}" class="card-img-top" alt="{{$movie->title}}">
         <div class="card-body">
         <h2 class="card-title">{{$movie->title}}</h2>
         <h4 class="card-subtitle my-2 text-muted">Titolo originale: {{$movie->original_title}}</h4>
-        <p class="card-text mt-5 mb-0">Paese di produzione: {{$movie->nationality}}</p>
-        <p class="card-text mb-5">Voto del pubblico: {{$movie->vote}} / 10</p>
+        <p class="card-text mt-2 mb-0">Paese di produzione: {{$movie->nationality}}</p>
+        <p class="card-text mb-2">Voto del pubblico: {{$movie->vote}} / 10</p>
 
         <a href="{{route('movie-details', ($movie->id-1))}}" class="btn-warning btn mt-3"><< Film precedente</a>
         <a href="{{route('movie-details', ($movie->id+1))}}" class="btn-info btn mt-3">Film successivo >></a><br>
